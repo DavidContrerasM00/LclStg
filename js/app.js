7a0965309a91de0f081cbabeaafe89e03b44b818
David Contreras Martinez <alfonsodac_18@alu.uabcs.mx>
2020-10-25 17:00:24 -0700

    Guds shit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const todoInput = document.querySelector(".todo-input");
 const btnAdd = document.querySelector("#btnAdd");
 const newTaskText = document.querySelector(".new-task-text");
 let index = 1;
+let tareas = [];
 
 btnAdd.addEventListener("click", () => {
 	todoInput.classList.remove("hidden")
@@ -10,9 +11,9 @@ btnAdd.addEventListener("click", () => {
 })
 
 todoInput.addEventListener("keypress", e => {
-	if(e.key === "Enter") {
+	if (e.key === "Enter") {
 		const task = e.target.value;
-		const taskElement =  makeTaskElement(task);
+		const taskElement = makeTaskElement(task);
 		const todoList = document.querySelector(".todo-list");
 		const taskCheckComplete = taskElement.querySelector("input[type=checkbox]");
 		const removeBtn = taskElement.querySelector("a");
@@ -27,14 +28,7 @@ todoInput.addEventListener("keypress", e => {
 	}
 })
 
-const makeTaskElement = function(task) {
-	/*const template = `<li>
-		<input type="checkbox">
-		<label for="">${task}</label>
-		<a href="#"></a>
-		</li>`;
-	document.querySelector('.todo-list').innerHTML += template;
-	//document.querySelector('.todo-list').textContent += template;*/
+const makeTaskElement = function (task) {
 	const li = document.createElement('li');
 	const checkbox = document.createElement('input')
 	const label = document.createElement('label')
@@ -48,14 +42,18 @@ const makeTaskElement = function(task) {
 	li.appendChild(label);
 	li.appendChild(a);
 
+	tareas[tareas.length]=task;
+
+	localStorage.setItem("Tarea", tareas);
+
 	return li
 }
 
-const bindTaskEvent = function(el, event, fn) {
+const bindTaskEvent = function (el, event, fn) {
 	el.addEventListener(event, fn)
 }
 
-const completeTask = function(e) {
+const completeTask = function (e) {
 	const parent = e.target.parentNode;
 	const labelTask = parent.querySelector("label");
 	labelTask.classList.toggle("task-complete")
@@ -63,7 +61,7 @@ const completeTask = function(e) {
 	taskCompletesCount.textContent = countTaskCompletes()
 }
 
-const deleteTask = function(e) {
+const deleteTask = function (e) {
 	const parent = e.target.parentNode;
 	const parentList = parent.parentNode;
 
@@ -72,7 +70,43 @@ const deleteTask = function(e) {
 	taskCompletesCount.textContent = countTaskCompletes()
 }
 
-const countTaskCompletes = function() {
+const countTaskCompletes = function () {
 	const totalTaskCompletes = document.querySelectorAll("label.task-complete")
 	return totalTaskCompletes.length;
 }
+
+function actLS() {
+	if(localStorage.getItem("Tarea"))
+	{
+		tareas = localStorage.getItem("Tarea")
+		
+		for (x in tareas) {
+
+			if(tareas[x]!=",")
+			{
+				const li = document.createElement('li');
+				const checkbox = document.createElement('input')
+				const label = document.createElement('label')
+				const a = document.createElement('a');
+		
+				checkbox.type = "checkbox";
+				label.textContent = tareas[x];
+				a.href = "#";
+		
+				li.appendChild(checkbox);
+				li.appendChild(label);
+				li.appendChild(a);
+		
+				const taskElement = li;
+		
+				const todoList = document.querySelector(".todo-list");
+				const taskCheckComplete = taskElement.querySelector("input[type=checkbox]");
+				const removeBtn = taskElement.querySelector("a");
+				bindTaskEvent(taskCheckComplete, "click", completeTask)
+				bindTaskEvent(removeBtn, "click", deleteTask)
+				todoList.prepend(taskElement)
+			}
+		}
+	}
+}
+actLS();
